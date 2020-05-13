46797ca4ddd798c17e03e607f6e776c8b16bc55c
voloshchenko1258 <65308790+voloshchenko1258@users.noreply.github.com>
2020-05-13 22:57:05 +0300

    voloshchenko1258
    sp_lab1

---
+++ sp_lab1/sp_lab1.2/sp_lab1.2.cpp
@@ -0,0 +1,101 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include <locale.h>
+
+const int BytesToRead = 2048;
+
+void FromUnicode(char*);
+void ToUnicode(char*);
+bool OpenAndCreateFiles(char*, HANDLE*, HANDLE*);
+
+void FromUnicode(char* path) {
+	HANDLE readFile;
+	HANDLE writeFile;
+
+	if (!OpenAndCreateFiles(path, &readFile, &writeFile))
+		return;
+
+	BYTE buffer[BytesToRead];
+	DWORD dwBytes;
+
+	while (ReadFile(readFile, buffer, BytesToRead, (LPDWORD)&dwBytes, NULL))
+	{
+		if (dwBytes == 0)
+			break;
+
+		int uLength = MultiByteToWideChar(CP_UTF8, 0, (LPCCH)buffer, dwBytes, NULL, 0);
+		wchar_t* uStr = new wchar_t[uLength];
+		MultiByteToWideChar(CP_UTF8, 0, (LPCCH)buffer, dwBytes, uStr, uLength);
+
+		int aLength = WideCharToMultiByte(CP_ACP, 0, uStr, uLength, NULL, 0, NULL, NULL);
+		char* aStr = (char*)calloc(aLength, sizeof(char));
+
+		WideCharToMultiByte(CP_ACP, 0, uStr, uLength, aStr, aLength, NULL, NULL);
+
+		if (!WriteFile(writeFile, aStr, aLength * sizeof(char), &dwBytes, NULL)) {
+			printf("Error of encoding");
+			break;
+		}
+	}
+	CloseHandle(readFile);
+	CloseHandle(writeFile);
+}
+
+void ToUnicode(char* path) {
+	HANDLE readFile;
+	HANDLE writeFile;
+
+	if (!OpenAndCreateFiles(path, &readFile, &writeFile))
+		return;
+
+	BYTE buffer[BytesToRead];
+	DWORD dwBytes;
+
+	while (ReadFile(readFile, buffer, BytesToRead, (LPDWORD)&dwBytes, NULL))
+	{
+		if (dwBytes == 0)
+			break;
+
+		int uLength = MultiByteToWideChar(CP_UTF8, 0, (LPCCH)buffer, dwBytes, NULL, 0);
+		wchar_t* uStr = (wchar_t*)calloc(uLength, sizeof(wchar_t));
+
+		MultiByteToWideChar(CP_UTF8, 0, (LPCCH)buffer, dwBytes, uStr, uLength);
+
+		if (!WriteFile(writeFile, uStr, uLength * sizeof(wchar_t), (LPDWORD)&dwBytes, NULL)) {
+			printf("Error of encoding");
+			break;
+		}
+	}
+}
+
+bool OpenAndCreateFiles(char* path, HANDLE* readFile, HANDLE* writeFile) {
+	*readFile = CreateFileA(path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	*writeFile = CreateFileA("result.txt", GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (*readFile == INVALID_HANDLE_VALUE || *writeFile == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+int main(int argc, char* argv[]) {
+
+	if (argc == 3) {
+		if (strcmp(argv[1], "-u") == 0) {
+			FromUnicode(argv[2]);
+		}
+		else if (strcmp(argv[1], "-a") == 0) {
+			ToUnicode(argv[2]);
+		}
+		else {
+			printf("Invalid arguments!");
+		}
+	}
+	else {
+		printf("Invalid arguments!");
+	}
+	return 0;
+}
---
+++ sp_lab1/sp_lab1/sp_lab1.1.cpp
@@ -0,0 +1,140 @@
+﻿// sp_lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <strsafe.h>
+#include <stdio.h>
+#include <tchar.h>
+
+#define DIV 1024
+#define WIDTH 7
+
+#define PRINT_ERROR 0
+#define PRINT_INFORMATION 1
+
+using namespace std;
+
+void ErrorExit(LPCWSTR lpszFunction){
+    // Retrieve the system error message for the last-error code
+
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError();
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
+
+    // Display the error message and exit the process
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+    StringCchPrintf((LPTSTR)lpDisplayBuf,
+        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+        TEXT("%s failed with error %d: %s"),
+        lpszFunction, dw, lpMsgBuf);
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+   // ExitProcess(dw);
+}
+
+void SystemInfo() {
+    SYSTEM_INFO siSysInfo;
+
+    // Copy the hardware information to the SYSTEM_INFO structure. 
+
+    GetSystemInfo(&siSysInfo);
+
+    // Display the contents of the SYSTEM_INFO structure. 
+
+    printf("Hardware information: \n");
+    printf("  OEM ID: %u\n", siSysInfo.dwOemId);
+    printf("  Number of processors: %u\n",
+        siSysInfo.dwNumberOfProcessors);
+    printf("  Page size: %u\n", siSysInfo.dwPageSize);
+    printf("  Processor type: %u\n", siSysInfo.dwProcessorType);
+    printf("  Minimum application address: %lx\n",
+        siSysInfo.lpMinimumApplicationAddress);
+    printf("  Maximum application address: %lx\n",
+        siSysInfo.lpMaximumApplicationAddress);
+    printf("  Active processor mask: %u\n",
+        siSysInfo.dwActiveProcessorMask);
+}
+
+void SystemMemoryInfo() {
+    MEMORYSTATUSEX statex;
+
+    statex.dwLength = sizeof(statex);
+
+    GlobalMemoryStatusEx(&statex);
+
+    _tprintf(TEXT("There is  %*ld percent of memory in use.\n"),
+        WIDTH, statex.dwMemoryLoad);
+    _tprintf(TEXT("There are %*I64d total KB of physical memory.\n"),
+        WIDTH, statex.ullTotalPhys / DIV);
+    _tprintf(TEXT("There are %*I64d free  KB of physical memory.\n"),
+        WIDTH, statex.ullAvailPhys / DIV);
+    _tprintf(TEXT("There are %*I64d total KB of paging file.\n"),
+        WIDTH, statex.ullTotalPageFile / DIV);
+    _tprintf(TEXT("There are %*I64d free  KB of paging file.\n"),
+        WIDTH, statex.ullAvailPageFile / DIV);
+    _tprintf(TEXT("There are %*I64d total KB of virtual memory.\n"),
+        WIDTH, statex.ullTotalVirtual / DIV);
+    _tprintf(TEXT("There are %*I64d free  KB of virtual memory.\n"),
+        WIDTH, statex.ullAvailVirtual / DIV);
+
+    // Show the amount of extended memory available.
+
+    _tprintf(TEXT("There are %*I64d free  KB of extended memory.\n"),
+        WIDTH, statex.ullAvailExtendedVirtual / DIV);
+}
+
+int main()
+{
+	for (;;) {
+		cout << "-e - print errors \n-s - print information about the system\nEnter your choice\n";
+		string choice;
+		cin >> choice;
+		int ichoice;
+		if (choice == "-e") ichoice = 0;
+		else if (choice == "-s") ichoice = 1;
+		else {
+			cout << "Incorrect data entered\n";
+			continue;
+		}
+
+		switch (ichoice)
+		{
+        case PRINT_ERROR:
+            if (!GetProcessId(NULL))
+                ErrorExit(TEXT("GetProcessId"));
+			break;
+		case PRINT_INFORMATION:
+            SystemInfo();
+            SystemMemoryInfo();
+			break;
+		default:
+			break;
+		}
+	}
+
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
